b08244676a1dd0c2d977627cfb9bc1919ac29cf0
Kamil <kamil.baran@kitopi.com>
2024-08-01 18:42:25 +0200

    feat: remove commented code

--- utils.js
+++ utils.js
@@ -1,10 +1,5 @@
 const { expect } = require('@playwright/test');
 
-// async function verifyItemVisible(page, itemName) {
-//   const itemLocator = page.locator(`text=${itemName}`);
-//   await expect(itemLocator).toBeVisible();
-// }
-
 async function verifyItemsVisible(page, ...itemNames) {
     for (const itemName of itemNames) {
       const itemLocator = page.locator(`text=${itemName}`);
